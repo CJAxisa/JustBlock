dd5779790b057a816aa795fb81b3fb94d9650baf
seank0h <seankoh754@gmail.com>
2018-10-20 13:54:19 -0400

    Made credits and added to main menu scene
    Very basic, functionality there just need to make it look better

---
+++ JustBlock/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("CJTestScene");
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+    public void BackToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
