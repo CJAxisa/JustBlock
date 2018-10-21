02f793175e0f78a626ec9bf8559f1ade148892d7
spalunki <axisac72@gmail.com>
2018-10-21 19:00:03 -0400

    Working on system Manager

--- JustBlock/Assets/Scripts/CollisionManager.cs
+++ JustBlock/Assets/Scripts/CollisionManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CollisionManager : MonoBehaviour {
 
     private InputManager IM;
+    private GameObject projectile;
 	// Use this for initialization
 	void Start () {
         IM =gameObject.GetComponent<InputManager>();
@@ -12,6 +13,14 @@ public class CollisionManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        projectile = GameObject.FindGameObjectWithTag("Fireball");
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Fireball")
+        {
+            Fireball.setHit(true);
+        }
+    }
 }
---
+++ JustBlock/Assets/Scripts/SystemManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour
+{
+
+    public enum matchStates 
+    {
+        GameOver,
+        BallinAction,
+        BallReset,
+        RPS
+    };
+    public static matchStates MS;
+    matchStates lastMS;
+    private InputManager IM;
+
+    // Use this for initialization
+    void Start()
+    {
+        MS = new matchStates();
+        IM = gameObject.GetComponent<InputManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (MS)
+        {
+            case matchStates.GameOver:
+                break;
+            case matchStates.BallinAction:
+                break;
+            case matchStates.BallReset:
+                break;
+            case matchStates.RPS:
+                break;
+            default:
+                break;
+        }
+
+        if (lastMS != MS)
+        {
+            if (lastMS == matchStates.BallinAction)
+                freezePlayers();
+            else if (MS == matchStates.BallinAction)
+                unfreezePlayers();
+        }
+
+        lastMS = MS;
+    }
+
+    void freezePlayers()
+    {
+        ///gameObject;
+    }
+
+    void unfreezePlayers()
+    {
+
+    }
+    public static matchStates getMatchStates()
+    {
+        return MS;
+    }
+}
