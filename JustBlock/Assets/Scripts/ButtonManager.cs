7d46bc027a2940047bb13be9639ca79124dfa881
Anthony <anthonykpichardo@gmail.com>
2018-10-21 19:22:51 -0400

    Control things

--- JustBlock/Assets/Scripts/ButtonManager.cs
+++ JustBlock/Assets/Scripts/ButtonManager.cs
@@ -37,7 +37,7 @@ public class ButtonManager : MonoBehaviour {
         gameObject.GetComponent<SceneManagement>().UnFreezePlayerOne();
     }
 
-    public void Controls()
+    public void BackToControls()
     {
         gameObject.GetComponent<SceneManagement>().controlsMenu.SetActive(true);
         gameObject.GetComponent<SceneManagement>().pauseMenu.SetActive(false);
